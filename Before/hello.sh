9f3ba7731e38b9d18c9fb89fc3e6f5412ac36acf
vamgutta <vamsi20@gmail.com>
2020-05-27 04:00:43 +0530

    XXXXX_code changed

--- Before/hello.sh
+++ Before/hello.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
 
-select name in vamsi john tom ben
-do
-	case $name in
-	vamsi )
-	  echo vamsi selected
-	  ;;
-	john )
-	  echo john selected
-	  ;;
-	tom )
-	  echo tom selected
-	  ;;
-	ben )
-	  echo ben selected
-	  ;;
-	* )
-	  echo "Error..please provide no b/w 1 to 4"
-	  ;;
-	esac
-done
+echo "Hallo Vamsi"
