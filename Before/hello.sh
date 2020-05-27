2fe5fcee8ecc940ef565e93e430907c43871785a
vamgutta <vamsi20@gmail.com>
2020-05-27 13:09:47 +0530

    no message

--- Before/hello.sh
+++ Before/hello.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-echo -n "Enter a number: "
-read VAR
 
-if [[ $VAR -gt 10 ]]
-then
-  echo -e "\e[31mThe variable is greater than 10."
-elif [[ $VAR -eq 10 ]]
-then
-  echo -e "\e[32mThe variable is equal to 10."
-else
-  echo -e "\e[33mThe variable is less than 10."
-fi
+echo -e "\e[31;42m Hello world! \e[0m "
