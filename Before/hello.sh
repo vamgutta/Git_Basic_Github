061bb47e5f03b22566671feb0480cf072387c7b8
vamgutta <vamsi20@gmail.com>
2020-05-27 13:44:03 +0530

    new file added and checking for color in hello.sh file

--- Before/hello.sh
+++ Before/hello.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 echo -e "\e[31;42m Hello world! \e[0m "
+
+echo -e "\e[31mWegen der Corona-Pandemie geltende Kontaktbeschränkungen in Deutschland sollen im Wesentlichen bis Ende Juni verlängert werden.\n \e[32m Darauf haben sich Vertreter von Bund und Ländern verständigt, \n \e[33m wie das Bundespresseamt in Berlin mitteilte. \n \e[34m Ab dem 6. Juni soll es allerdings weitere Lockerungen geben - etwa, dass sich künftig bis zu zehn Menschen in der Öffentlichkeit treffen dürfen. \n \e[35m Dem Beschluss liege die Einschätzung zugrunde, dass die Zahl der Neuinfektionen mit dem Coronavirus in Deutschland auch einen Monat nach Beginn der Lockerungsmaßnahmen auf niedrigem Niveau sei, so das Bundespresseamt. \e[0m"
