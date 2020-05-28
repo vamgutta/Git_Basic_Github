8805644add6374c0d779b848782283e23520eaa5
vamgutta <vamsi20@gmail.com>
2020-05-28 15:07:30 +0530

    updated file

--- Before/robo-shop-project/check.sh
+++ Before/robo-shop-project/check.sh
@@ -1,3 +1,13 @@
 #!/bin/bash 
 
-exit 20
+RB="\e[0m\e[1;31m"
+GB="\e[0m\e[1;32m"
+YB="\e[0m\e[1;33m"
+BB="\e[0m\e[1;34m"
+MB="\e[0m\e[1;35m"
+CB="\e[0m\e[1;36m"
+N="\e[0m"
+D="\e[0m\e[2m"
+B="\e[0m\e[1m"
+
+echo -e "RB[ERROR]GB[SUCCESS]YB[WARNING]"
